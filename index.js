872784057460be458b227c5813f729577a83c8a0
Jonathan Warner <jewarner57@gmail.com>
2021-05-12 19:17:18 -0400

    feat: add make, model, and city

--- index.js
+++ index.js
@@ -4,22 +4,42 @@ var userData = require('./data.json');
 function printUserData(userData) {
     userData.forEach(function (user) {
         //  Challenge 1
-        console.log(capitalize(user.first_name));
-        console.log(capitalize(user.last_name));
+        var firstName = capitalize(user.first_name);
+        var lastName = capitalize(user.last_name);
+        console.log(firstName + " " + lastName);
+        // Make and Model
+        var make = titleCase(user.make);
+        var model = titleCase(user.model);
+        console.log(make + " " + model);
         // Challenge 2
         var purchasedDate = new EasyDate(user.purchased);
-        console.log(purchasedDate.format('%B %D, %Y'));
+        console.log(purchasedDate.format('Purchased: %B %D, %Y'));
         // Challenge 3
         var lastPaymentDate = new EasyDate(user.lastpayment);
         var timeAgo = new EasyDate().when(lastPaymentDate);
         console.log("Last Payment: " + timeAgo);
         // Challenge 4
-        console.log(formatPhoneNumer(user.phone));
+        console.log("Phone: " + formatPhoneNumer(user.phone));
+        // City name
+        console.log("City: " + titleCase(user.city));
+        // Add trailing newline
+        console.log();
     });
 }
 // Uppercase the first letter of a string
 function capitalize(str) {
     return "" + str[0].toUpperCase() + str.slice(1);
+}
+// Uppercase the first letter in each word of a string
+function titleCase(str) {
+    // split words into array
+    var words = str.split(' ');
+    // capitalize each word
+    var titledWords = words.map(function (word) {
+        return capitalize(word);
+    });
+    // return the new string
+    return titledWords.join(' ');
 }
 // Takes a 10 digit phone number string and formats it into (000) 000-0000 format
 function formatPhoneNumer(phoneNum) {
@@ -31,4 +51,5 @@ function formatPhoneNumer(phoneNum) {
     var Subscriber = phoneNum.substring(6, 10);
     return "(" + AreaCode + ") " + Prefix + "-" + Subscriber;
 }
-module.exports = { formatPhoneNumer: formatPhoneNumer, printUserData: printUserData, capitalize: capitalize };
+printUserData(userData);
+module.exports = { formatPhoneNumer: formatPhoneNumer, printUserData: printUserData, capitalize: capitalize, titleCase: titleCase };
--- index.ts
+++ index.ts
@@ -19,12 +19,18 @@ function printUserData(userData: User[]): void {
   userData.forEach((user: User): void => {
 
     //  Challenge 1
-    console.log(capitalize(user.first_name))
-    console.log(capitalize(user.last_name))
+    const firstName: string = capitalize(user.first_name)
+    const lastName: string = capitalize(user.last_name)
+    console.log(`${firstName} ${lastName}`)
+
+    // Make and Model
+    const make: string = titleCase(user.make)
+    const model: string = titleCase(user.model)
+    console.log(`${make} ${model}`)
 
     // Challenge 2
     const purchasedDate: typeof EasyDate = new EasyDate(user.purchased)
-    console.log(purchasedDate.format('%B %D, %Y'))
+    console.log(purchasedDate.format('Purchased: %B %D, %Y'))
 
     // Challenge 3
     const lastPaymentDate: typeof EasyDate = new EasyDate(user.lastpayment)
@@ -32,7 +38,13 @@ function printUserData(userData: User[]): void {
     console.log(`Last Payment: ${timeAgo}`)
 
     // Challenge 4
-    console.log(formatPhoneNumer(user.phone))
+    console.log(`Phone: ${formatPhoneNumer(user.phone)}`)
+
+    // City name
+    console.log(`City: ${titleCase(user.city)}`)
+
+    // Add trailing newline
+    console.log()
   })
 }
 
@@ -41,17 +53,33 @@ function capitalize(str: string): string {
   return `${str[0].toUpperCase()}${str.slice(1)}`
 }
 
+// Uppercase the first letter in each word of a string
+function titleCase(str: string): string {
+  // split words into array
+  const words: string[] = str.split(' ')
+
+  // capitalize each word
+  const titledWords: string[] = words.map((word: string): string => {
+    return capitalize(word)
+  })
+
+  // return the new string
+  return titledWords.join(' ')
+}
+
 // Takes a 10 digit phone number string and formats it into (000) 000-0000 format
 function formatPhoneNumer(phoneNum: string): string {
   if (phoneNum.length != 10) {
     return `Phone number: ${phoneNum} is not valid.`
   }
 
-  const AreaCode = phoneNum.substring(0, 3)
-  const Prefix = phoneNum.substring(3, 6)
-  const Subscriber = phoneNum.substring(6, 10)
+  const AreaCode: string = phoneNum.substring(0, 3)
+  const Prefix: string = phoneNum.substring(3, 6)
+  const Subscriber: string = phoneNum.substring(6, 10)
 
   return `(${AreaCode}) ${Prefix}-${Subscriber}`
 }
 
-module.exports = { formatPhoneNumer, printUserData, capitalize }
+printUserData(userData)
+
+module.exports = { formatPhoneNumer, printUserData, capitalize, titleCase }
