4bec74c45754f6e84ff7fe28e30bb23e12157fb9
Jonathan Warner <jewarner57@gmail.com>
2021-05-12 18:16:13 -0400

    feat: challenge 2

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const EasyDate = require('@jewarner57/easydate')
+const EasyAsString = require('@jewarner57/easy-as-string')
 // Challenge 0
 const userData = require('./data.json')
 
-console.log(userData[0])
+userData.forEach((user) => {
+  //  Challenge 1
+  console.log(user.first_name.capitalize())
+  console.log(user.last_name.capitalize())
+})
